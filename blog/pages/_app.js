5ffee5dfe5533b2ae33ac40e49db36112c8370ab
scw1997 <1091479495@qq.com>
2020-11-14 20:00:09 +0800

    下

--- blog/pages/_app.js
+++ blog/pages/_app.js
@@ -1,5 +1,5 @@
 import './global.scss'
-// import 'antd/dist/antd.css'
+import 'antd/dist/antd.min.css'
 import React from 'react'
 import {Provider} from 'react-redux'
 import Store from '../store/store'
